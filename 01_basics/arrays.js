91554444a47a7ac25a2bc53bc3fe8b2d8aeba7e6
suyash700 <139065167+suyash700@users.noreply.github.com>
2024-12-23 06:51:59 +0000

    arrays

---
+++ 01_basics/arrays.js
@@ -0,0 +1,18 @@
+const arr=["hello","i","suyash"]
+
+arr.push(2);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(9)//inserted at start
+
+console.log(arr);
+
+arr.shift()
+console.log(arr);
+
+const newArr=arr.join()
+console.log(newArr);
+console.log(typeof newArr);
+
+//slice splice
