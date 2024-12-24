509cee9e6965486f6c1a8caf89f4604c76fb19e7
suyash700 <139065167+suyash700@users.noreply.github.com>
2024-12-24 18:17:49 +0000

    html1

---
+++ 04_dom/dom1.js
@@ -0,0 +1,45 @@
+/**
+ * document.getElementById('title')
+ * document.getElementById('title').id
+ * =>title
+ * 
+ * document.getElementById('title').getAttribute('class')
+ * =>heading
+ * 
+ * document.getElementById('title').setAttribute('class','test')
+ * override earlier name
+ * 
+ * 
+ *const title= document.getElementById('title').id
+ *
+ * title.innerText
+ * 'DOM learning on Chai aur code'
+ * <h1  id="title" class="heading">DOM learning on 
+ *   Chai aur code <span style="display: none;      ">test text</span></h1>
+ * 
+ *  title.textContent
+ * 'DOM learning on Chai aur code test text'
+ */
+
+ /**
+  * 
+  * selecting items
+  * document.querySelector('h2')
+  *  <h2>Lorem ipsum dolor sit.</ 
+  *   h2>
+        <h2>Lorem ipsum dolor sit.</h2>
+        <h2>Lorem ipsum dolor sit.</h2>
+
+
+        nodelist
+
+        access elements
+
+        store=>then apply for each
+        arr[index].for
+
+
+        //HTMLcolletions to array comvert
+
+        Array.from()
+  */
