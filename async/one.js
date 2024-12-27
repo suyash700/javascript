aac3e69df311dc10ea5daf8553c30f14ec1da478
suyash700 <139065167+suyash700@users.noreply.github.com>
2024-12-27 17:15:52 +0000

    info event

---
+++ async/one.js
@@ -0,0 +1,18 @@
+
+
+/*  
+     [database]
+read,write of data in file
+
+sync => (blocking)
+1.read the file
+2.then execute remaining code
+
+
+async(non-blocking) =>
+    1.execute i will do my job
+
+ex: user se input
+    store it in database(evaluate)
+    then successfully registered msg
+*/
